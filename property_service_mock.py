6cde00d83830f0b9e1a55ed44df3346ce413fa71
Bárbara Galiza <barnobregali02@gmail.com>
2024-04-27 20:11:22 +0100

    delta calculus done

--- property_service_mock.py
+++ property_service_mock.py
@@ -13,8 +13,24 @@ from ProjectUtils.MessagingService.schemas import (
     from_json
 )
 
+"""
+[{
+    "id": "661a7e5ab7bd0512178cf014",
+    "latitude": 40.639337,
+    "longitude": -8.65099,
+    "bathrooms": 2,
+    "bedrooms": 3,
+    "beds": 4,
+    "number_of_guests": 4,
+    "num_amenities": 4
+},
+...
+]
+"""
+
 # needs to be in this order, given train data was in this order
 class PropertyForAnalytics(BaseModel):
+    id: str
     latitude: float
     longitude: float
     bathrooms: int
@@ -26,6 +42,7 @@ class PropertyForAnalytics(BaseModel):
 
 properties = [
     PropertyForAnalytics(
+        id="661a7e5ab7bd0512178cf014",
         latitude=40.639337,
         longitude=-8.65099,
         bathrooms=2,
@@ -35,6 +52,7 @@ properties = [
         num_amenities=4
     ),
     PropertyForAnalytics(
+        id="1f994177dc45954c6148086",
         latitude=40.649127,
         longitude=-8.65455,
         bathrooms=1,
@@ -44,6 +62,7 @@ properties = [
         num_amenities=3
     ),
     PropertyForAnalytics(
+        id="60a283db136301428a0ae06",
         latitude=40.63892,
         longitude=-8.65459,
         bathrooms=1,
@@ -53,6 +72,7 @@ properties = [
         num_amenities=2
     ),
     PropertyForAnalytics(
+        id="1ac9af963e829344c53956b",
         latitude=40.64131,
         longitude=-8.65431,
         bathrooms=2,
@@ -62,6 +82,7 @@ properties = [
         num_amenities=5
     ),
     PropertyForAnalytics(
+        id="2f224177dc45954c6148086",
         latitude=40.64243,
         longitude=-8.64571,
         bathrooms=2,
@@ -71,6 +92,7 @@ properties = [
         num_amenities=6
     ),
     PropertyForAnalytics(
+        id="1e8b214c28d15241902904a",
         latitude=40.63889,
         longitude=-8.64554,
         bathrooms=2,
@@ -80,6 +102,7 @@ properties = [
         num_amenities=7
     ),
     PropertyForAnalytics(
+        id="f91622341c0651489309cfa",
         latitude=40.63848,
         longitude=-8.65434,
         bathrooms=3,
@@ -89,6 +112,7 @@ properties = [
         num_amenities=8
     ),
     PropertyForAnalytics(
+        id="87e1dfd41f80d24d1e1845d",
         latitude=40.63956,
         longitude=-8.65434,
         bathrooms=3,
@@ -100,7 +124,7 @@ properties = [
 ]
 
 def request_recommended_price():
-    json_properties = [property.dict() for property in properties]
+    json_properties = [property.model_dump() for property in properties]
     message = MessageFactory.create_get_recommended_price(json_properties)
     channel.basic_publish(
         exchange=EXCHANGE_NAME,
@@ -114,7 +138,7 @@ def request_recommended_price():
         if method_frame:
             message = from_json(body)
             print("Received message:\n" + str(message.__dict__))
-            print("Median price: " + str(message.body["median"]))
+            print("Recommended prices for each property: " + str(message.body))
             break
     
 request_recommended_price()    
